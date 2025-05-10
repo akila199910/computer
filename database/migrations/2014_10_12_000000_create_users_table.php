ea7b7a4d9093a17b641f6ad766e53590433380c1
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2025-05-10 20:32:31 +0530

    install data table spetie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('name');
+            $table->string('contact')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status');
+            $table->integer('password_set');
+            $table->string('ref_no')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
